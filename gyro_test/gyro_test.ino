74fe654f3eb619ac0773647629b51822cf35400c
shorge37 <146264784+shelmingeorge@users.noreply.github.com>
2025-05-06 00:20:26 +0300

    Update gyro_test.ino

--- gyro_test/gyro_test.ino
+++ gyro_test/gyro_test.ino
@@ -1,22 +1,22 @@
 #include <Wire.h>
 
-const int gyro_adress = 0x68;
+const int gyro_address = 0x68;
 double axis_speed[] = {0, 0, 0}, axis_accel[] = {0, 0, 0}; // x, y, z
 int raw_gyro_data[7];
 
 void gyro_setup(){
-  Wire.beginTransmission(gyro_adress);
-  Wire.write(gyro_adress);
+  Wire.beginTransmission(gyro_address);
+  Wire.write(gyro_address);
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   return;
   }
 
 void get_raw_gyro(){
-  Wire.beginTransmission(gyro_adress);
+  Wire.beginTransmission(gyro_address);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(gyro_adress, 14, true); // request a total of 14 registers
+  Wire.requestFrom(gyro_address, 14, true); // request a total of 14 registers
   for (byte i = 0; i < 7; i++) raw_gyro_data[i] = Wire.read() << 8 | Wire.read();
 
   return;
