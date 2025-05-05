92ce5700aefed2124433c28ead43dbed3d927190
shorge37 <146264784+shelmingeorge@users.noreply.github.com>
2025-05-05 21:48:36 +0300

    Create light_brightness_sensor_test.ino

---
+++ light_brightness_sensor_test/light_brightness_sensor_test.ino
@@ -0,0 +1,25 @@
+#include <BH1750.h>
+
+BH1750 lux_sensor(0x23);
+
+float get_lux_sensor(){
+  float lux = lux_sensor.readLightLevel();
+  return lux;
+  }
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Serial.println("START PROGRAM");
+
+  lux_sensor.begin();
+  }
+
+void loop() {
+  Serial.print("Light: ");
+  Serial.print(get_lux_sensor());
+  Serial.println(" lx");
+  
+  delay(300);
+}
